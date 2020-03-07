8c17ae9f0e525c12c176b66c39a220b7bacc1418
Ajayjyothish <ajayjyothish007@gmail.com>
2020-03-07 17:56:45 +0530

    updated dialogflow file

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-
-const uuid = require('uuid');
-const fs = require('fs');
-const util = require('util');
-const {struct} = require('pb-util');
+const uuid = require("uuid");
+const fs = require("fs");
+const util = require("util");
+const { struct } = require("pb-util");
 // Imports the Dialogflow library
-const dialogflow = require('dialogflow');
+const dialogflow = require("dialogflow");
+const express = require("express");
+const http = require("http");
+const multer = require("multer");
 
-// // Instantiates a session client
-// const sessionClient = new dialogflow.SessionsClient();
-//
-// // The path to identify the agent that owns the created intent.
-// const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-//
-// // Read the content of the audio file and send it as part of the request.
-//
-//
-// // Recognizes the speech in the audio and detects its intent.
-// const [response] = await sessionClient.detectIntent(request);
-//
-// console.log('Detected intent:');
-// const result = response.queryResult;
-// // Instantiates a context client
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + ".wav"); //Appending .jpg
+  }
+});
 
+const upload = multer({ storage: storage });
+var type = upload.single("audio_data");
 
-async function runSample(projectId = 'test1-lgbyty') {
+async function runSample(audio_file, projectId = "test1-lgbyty") {
   // A unique identifier for the given session
   const sessionId = uuid.v4();
 
@@ -35,22 +32,22 @@ async function runSample(projectId = 'test1-lgbyty') {
 
   // The audio file query request.
   const readFile = util.promisify(fs.readFile);
-  const inputAudio = await readFile("sample.wav");
+  const inputAudio = await readFile(`public/uploads/${audio_file}`);
   const request = {
     session: sessionPath,
     queryInput: {
       audioConfig: {
-        audioEncoding:'AUDIO_ENCODING_LINEAR_16',
-        sampleRateHertz: "16000",
-        languageCode: "en-US",
-      },
+        audioEncoding: "AUDIO_ENCODING_LINEAR_16",
+        sampleRateHertz: "48000",
+        languageCode: "en-US"
+      }
     },
-    inputAudio: inputAudio,
+    inputAudio: inputAudio
   };
 
   // Send request and log result
   const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
+  console.log("Detected intent");
   const result = responses[0].queryResult;
   console.log(`  Query: ${result.queryText}`);
   console.log(`  Response: ${result.fulfillmentText}`);
@@ -61,4 +58,18 @@ async function runSample(projectId = 'test1-lgbyty') {
   }
 }
 
-runSample()
+const hostname = "localhost";
+const port = 3005;
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.post("/", type, (req, res) => {
+  console.log(req.file);
+  runSample(req.file.filename);
+});
+
+server.listen(port, hostname, () => {
+  console.log("Server is running");
+});
